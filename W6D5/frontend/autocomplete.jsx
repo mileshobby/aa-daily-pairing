1f6693a589e3421682a3c5f4051a6d6a4baba2b3
Miles Hobby <mhobby16@gmail.com>
2017-05-07 13:50:33 -0700

    fix controlled component bug to filter names once autocomplete used

--- W6D5/frontend/autocomplete.jsx
+++ W6D5/frontend/autocomplete.jsx
@@ -4,42 +4,41 @@ class AutoComplete extends React.Component {
   constructor({names}) {
     super();
     this.names = names;
-    this.state = {matchedNames: names, inputVal: ""};
+    this.state = {inputVal: ""};
     this.findNames = this.findNames.bind(this);
-    this.nameList = this.nameList.bind(this);
+    this.handleInput = this.handleInput.bind(this);
     this.complete = this.complete.bind(this);
   }
 
-  findNames(e){
-    let input = e.target.value;
+  findNames(){
+    let input = this.state.inputVal;
+
     let matched;
     if (input === "") {
       matched = this.names;
     } else {
       matched = this.names.filter((name)=>name.startsWith(input));
     }
-    this.setState({matchedNames: matched});
+    return matched;
   }
 
-  nameList(){
-    return this.state.matchedNames.map((name, i)=>{
-      return <li onClick={this.complete} key={name}>{name}</li>;
-    });
+  handleInput(e){
+    this.setState({inputVal: e.target.value});
   }
 
   complete(e){
     this.setState({inputVal: e.target.innerHTML});
-    const input = document.getElementById("input");
-    input.value = e.target.innerHTML;
   }
 
   render(){
+    const matches = this.findNames().map(
+          (name)=>(<li onClick={this.complete} key={name}>{name}</li>));
     return (
       <div className="widget" id="autocomplete">
         <h1>Autocomplete</h1>
-        <input id="input" type="text" onChange={this.findNames} placeholder='Search...'></input>
+        <input id="input" type="text" onChange={this.handleInput} value={this.state.inputVal} placeholder='Search...'></input>
         <ul>
-          {this.nameList()}
+          {matches}
         </ul>
       </div>
     );
